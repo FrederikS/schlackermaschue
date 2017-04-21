5a7ebd4db94a4a2c1f898a49eddc7dc67d5aef01
frederik.steffen <frederik.steffen@gmail.com>
2017-04-21 18:10:37 +0200

    added page transition

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -2,37 +2,90 @@
 import React from 'react';
 import Helmet from 'react-helmet';
 import { config } from 'config';
+import { browserHistory } from 'react-router';
+import CSSTransitionGroup from 'react-transition-group/CSSTransitionGroup';
 import Header from './header';
 import Footer from './footer';
+import '../../../css/transition.css';
 
 type Props = {
   children: React.Element<any>,
   page: ?Page,
+  location: RouteLocation,
 };
 
-const Layout = ({ children, page }: Props): React.Element<any> => {
-  const defaultMetaData = {
-    title: config.blogTitle,
-    description: config.blogDescription,
-    keywords: 'blog',
-  };
-  const { title, description, keywords } = page ? page.data : defaultMetaData;
-  return (
-    <div>
-      <Helmet
-        title={title}
-        meta={[
-          { name: 'description', content: description },
-          { name: 'keywords', content: keywords },
-        ]}
-      />
-      <Header withHomeLink={page ? page.path !== '/' : true} />
-      <main role="main">
-        {children}
-      </main>
-      <Footer />
-    </div>
-  );
+type State = {
+  currentLocation: RouteLocation,
+  routeChanged: boolean,
 };
+
+class Layout extends React.Component {
+  props: Props;
+  state: State;
+
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      currentLocation: props.location,
+      routeChanged: false,
+    };
+  }
+
+  componentDidMount() {
+    browserHistory.listen((location: RouteLocation) => {
+      if (location.pathname !== this.state.currentLocation.pathname) {
+        this.setState(
+          {
+            currentLocation: location,
+            routeChanged: true,
+          },
+          () => {
+            setTimeout(() => {
+              this.setState({
+                routeChanged: false,
+              });
+            }, 150);
+          }
+        );
+      }
+    });
+  }
+
+  render() {
+    const defaultMetaData = {
+      title: config.blogTitle,
+      description: config.blogDescription,
+      keywords: 'blog',
+    };
+    const { children, page } = this.props;
+    const { title, description, keywords } = page ? page.data : defaultMetaData;
+    return (
+      <div>
+        <Helmet
+          title={title}
+          meta={[
+            { name: 'description', content: description },
+            { name: 'keywords', content: keywords },
+          ]}
+        />
+        <Header withHomeLink={page ? page.path !== '/' : true} />
+        <main role="main">
+          <CSSTransitionGroup
+            transitionName="move"
+            transitionEnterTimeout={500}
+            transitionLeaveTimeout={300}
+          >
+            {this.state.routeChanged
+              ? null
+              : <div key={this.state.currentLocation}>
+                  {children}
+                </div>}
+          </CSSTransitionGroup>
+        </main>
+        <Footer />
+      </div>
+    );
+  }
+}
 
 export default Layout;
