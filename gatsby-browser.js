40920d23e7128cbc13df59138dfcf90b30ebcc78
frederik.steffen <frederik.steffen@gmail.com>
2017-04-23 15:10:52 +0200

    added react-ga

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,15 @@
+import ReactGA from 'react-ga';
+import { config } from 'config';
+
+ReactGA.initialize(config.gaTrackingCode);
+
+export const onRouteUpdate = (route: RouteLocation) => {
+  ReactGA.pageview(route.pathname);
+};
+
 export const shouldUpdateScroll = () => false;
 
 export default {
+  onRouteUpdate,
   shouldUpdateScroll,
 };
