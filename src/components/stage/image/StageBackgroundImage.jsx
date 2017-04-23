f3d90f08da523ffae4a5ec4426762049027c7ce7
frederik.steffen <frederik.steffen@gmail.com>
2017-04-23 14:19:10 +0200

    fixed position for parallax

--- src/components/stage/image/StageBackgroundImage.jsx
+++ src/components/stage/image/StageBackgroundImage.jsx
@@ -3,7 +3,11 @@ import React from 'react';
 import styled from 'styled-components';
 import { Track, TrackDocument } from 'react-track';
 import { tween } from 'react-imation';
-import { bottomTop, calculateScrollY } from 'react-track/tracking-formulas';
+import {
+  topTop,
+  bottomTop,
+  calculateScrollY,
+} from 'react-track/tracking-formulas';
 import { translate3d } from 'react-imation/tween-value-factories';
 
 const BackgroundImageWrapper = styled.div`
@@ -41,15 +45,24 @@ type Props = {
 
 const StageBackgroundImage = ({ image, children }: Props) => (
   <BackgroundImageWrapper>
-    <TrackDocument formulas={[calculateScrollY, bottomTop]}>
-      {(scrollY, bottomTopOuter) => (
-        <Track component={BackgroundImage} formulas={[bottomTopOuter]}>
-          {(BackgroundImageTracked, posBottomTop) => (
+    <TrackDocument
+      updateOnDidMount
+      formulas={[calculateScrollY, topTop, bottomTop]}
+    >
+      {(scrollY, topTopOuter, bottomTopOuter) => (
+        <Track
+          component={BackgroundImage}
+          formulas={[topTopOuter, bottomTopOuter]}
+        >
+          {(BackgroundImageTracked, posTopTop, posBottomTop) => (
             <BackgroundImageTracked
               image={image}
               style={tween(scrollY, [
-                [0, { transform: translate3d(0, 0, 0) }],
-                [posBottomTop, { transform: translate3d(0, 100, 0) }],
+                [posTopTop, { transform: translate3d(0, 0, 0), opacity: 1 }],
+                [
+                  posBottomTop,
+                  { transform: translate3d(0, 100, 0), opacity: 0.5 },
+                ],
               ])}
             >
               {children}
